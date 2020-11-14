ab653071035939c66bd16725386aa3e9eda2fe3c
SONGHANSHI <973305062@qq.com>
2020-11-14 23:34:11 +0800

    lab8

--- app/Controllers/Travel.php
+++ app/Controllers/Travel.php
@@ -4,16 +4,65 @@ namespace App\Controllers;
 
 class Travel extends BaseController {
 
-public function index() {
-$places = new \App\Models\Places();
-$records = $places->findAll();
-$parser = \Config\Services::parser();
-return $parser->setData(['records' => $records])->render('placeslist');
-}
-public function showme($id) { 
-$places = new \App\Models\Places();
-$record = $places->find($id);
-$parser = \Config\Services::parser(); 
-return $parser->setData($record)->render('oneplace'); 
-}
-}
+    public function index() {
+        $places = new \App\Models\Places();
+        $records = $places->findAll();
+        $parser = \Config\Services::parser();
+        return $parser->setData(['records' => $records])->render('placeslist');
+
+        $table = new \CodeIgniter\View\Table();
+        $headings = $places->fields;
+        $displayHeadings = array_slice($headings, 1, 2);
+        $table->setHeading(array_map('ucfirst', $displayHeadings));
+        foreach ($records as $record) {
+            $nameLink = anchor("travel/showme/$record->id", $record->name);
+            $table->addRow($nameLink, $record->description);
+        }
+        $template = ['table_open' => '<table cellpadding="5px">', 'cell_start' => '<td style="border: 1px solid #dddddd;">', 'row_alt_start' => '<tr style="background-color:#dddddd">',];
+        $fields = ['title' => 'Travel Destinations', 'heading' => 'Travel Destinations', 'footer' => 'Copyright Xavier'];
+        $table->setTemplate($template);
+        return $parser->setData($fields)->render('templates\top') . $table->generate() . $parser->setData($fields)->render('templates\bottom');
+    }
+
+ public function showme($id) {
+        // connect to the model
+        $places = new \App\Models\Places();
+        // retrieve all the records
+        $record = $places->find($id);
+
+        $table = new \CodeIgniter\View\Table();
+        $headings = $places->fields;
+        $table->addRow($headings[0], $record['id'])
+                ->addRow($headings[1], $record['name'])
+                ->addRow($headings[2], $record['description'])
+                ->addRow($headings[3], $record['link'])
+                ->addRow($headings[4], "<img src=\"/image/".$record['image']."\"/>");
+
+        $template = [
+            'table_open' => '<table cellpadding="5px">',
+            'cell_start' => '<td style="border: 1px solid #dddddd;">',
+            'row_alt_start' => '<tr style="background-color:#dddddd">',
+        ];
+        $table->setTemplate($template);
+
+        $fields = [
+            'title' => 'Travel Destination',
+            'heading' => 'Travel Destination',
+            'footer' => 'Copyright SonHan Shi'
+        ];
+
+        // get a template parser
+        $parser = \Config\Services::parser();
+        return $parser->setData($fields)
+                ->render('templates\top') .
+                $table->generate() .
+                $parser->setData($fields)
+                ->render('templates\bottom');
+        /*
+        // tell it about the substitions
+        return $parser->setData($record)
+                // and have it render the template with those
+                ->render('oneplace');
+         */
+    }
+} 
